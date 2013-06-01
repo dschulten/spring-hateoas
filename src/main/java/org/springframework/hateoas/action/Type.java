4f7c0c7843c950808540090ea70f5512bef02cc1
Dietrich Schulten <ds@escalon.de>
2013-06-01 12:26:05 +0200

    Added some comments

--- src/main/java/org/springframework/hateoas/action/Type.java
+++ src/main/java/org/springframework/hateoas/action/Type.java
@@ -1,10 +1,22 @@
 package org.springframework.hateoas.action;
 
+/**
+ * Specifies explicit HTML5 input types.
+ * 
+ * @author Dietrich Schulten
+ * 
+ */
 public enum Type {
-	/** Determine type text or number, depending on the annotated parameter */
-	AUTO(null), TEXT("text"), HIDDEN("hidden"), PASSWORD("password"), COLOR("color"), DATE("date"), DATETIME("datetime"), DATETIME_LOCAL(
-			"datetime-local"), EMAIL("email"), MONTH("month"), NUMBER("number"), RANGE("range"), SEARCH("search"), TEL("tel"), TIME(
-			"time"), URL("url"), WEEK("week");
+	/** Determine input type text or number automatically, depending on the annotated parameter */
+	AUTO(null),
+	/** input type text */
+	TEXT("text"),
+	/** input type hidden */
+	HIDDEN("hidden"),
+	/** input type password */
+	PASSWORD("password"), COLOR("color"), DATE("date"), DATETIME("datetime"), DATETIME_LOCAL("datetime-local"), EMAIL(
+			"email"), MONTH("month"), NUMBER("number"), RANGE("range"), SEARCH("search"), TEL("tel"), TIME("time"), URL("url"), WEEK(
+			"week");
 
 	private String value;
 
